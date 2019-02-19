eec1dfad3e2f817f885a2f62305faa3de161b7d1
Farnaz Baksh <farnaz.baksh@gmail.com>
2019-02-19 14:44:49 -0400

    Flag update

--- SouthSudanFlag.c
+++ SouthSudanFlag.c
@@ -90,14 +90,35 @@ void doDisplay() {
 		glVertex3fv(p20); 
 	glEnd();
 	
+	//Triangle
 	glBegin(GL_POLYGON); 
-	
 	float triangleheight = sqrt((myheight*myheight) - ((myheight/2)*(myheight/2)));
-	
 		glColor3f(0.0, 0.0, 1.0);   // blue 
 		glVertex3f(0.0, myheight, 0.0); 
 		glVertex3f(triangleheight, myheight/2, 0.0); 
 		glVertex3f(0.0, 0.0, 0.0); 
+	glEnd();
+
+	//Star
+double thirty_degrees = 30 * (M_PI / 180);
+double eighteen_degrees = 18 * (M_PI / 180);
+double thirty_six_degrees = 36 * (M_PI / 180);
+	
+float radius = 17;
+float x_distance_to_center_of_star = (myheight / 2) * tan(thirty_degrees);
+float inner_radius_of_pentagon = 17 * sin(eighteen_degrees);
+float height_of_small_triangle = radius - inner_radius_of_pentagon;
+float outer_radius_of_pentagon = sqrt((inner_radius_of_pentagon*inner_radius_of_pentagon)+(((2 * (height_of_small_triangle * tan(eighteen_degrees)))/2)*((2 * (height_of_small_triangle * tan(eighteen_degrees)))/2)));
+float side_of_pentagon = 2 * (height_of_small_triangle * tan(eighteen_degrees));
+float side_of_star = sqrt((height_of_small_triangle * height_of_small_triangle) + (side_of_pentagon * side_of_pentagon));
+float x = side_of_star * cos(thirty_six_degrees);
+float y = side_of_star * sin(thirty_six_degrees);
+	
+	glBegin(GL_TRIANGLES); 
+		glColor3f(0.9,0.87,0.04);
+		glVertex2f(x_distance_to_center_of_star + outer_radius_of_pentagon, (myheight / 2));
+		glVertex2f(x_distance_to_center_of_star - inner_radius_of_pentagon, (myheight / 2) - (side_of_star + (side_of_pentagon / 2))); 
+		glVertex2f(x_distance_to_center_of_star - inner_radius_of_pentagon, (myheight / 2) + (side_of_star + (side_of_pentagon / 2)));	
 	glEnd();
 
 	glFlush();
@@ -108,7 +129,7 @@ int main(int argc, char *argv[]) {
 	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
 	glutInitWindowSize(mywidth*2,myheight*2);
 	glutInitWindowPosition(300,100);
-	glutCreateWindow("CSE3200: Simple Line : Farnaz Baksh)");
+	glutCreateWindow("CSE3200: Simple Line : A Team)");
 	doInit();
 	glutDisplayFunc(doDisplay);
 	glutMainLoop();
